499f27f3b5229660c960c6d8e8cd6cafbe1728b8
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-09-07 15:24:33 +0530

    routes added

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
 //const http = require('http')  express will do it internally
 const express = require('express')
 const bodyParser = require('body-parser')
-
 const app = express()
-
 app.use(bodyParser.urlencoded({extended:false}))
+const adminRouter = require('./routes/admin.js')
+const shopRouter = require('./routes/shop.js')
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="name" placeholder="Item Name"><br><input type="number" name="size" placeholder="size"><br><button type="submit">ADD</button></form>')
+app.use(adminRouter)
+app.use(shopRouter)
+app.use((req,res,next)=>{
+    res.status('404').send('<h1>ERROR 404 : Page Not Found</h1>')
 })
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Welcome to express</h1>')
-})
-//const server = http.createServer(app)
-
-//server.listen(4000)    Express app can listen to requests by using http internally
 app.listen(4000)
