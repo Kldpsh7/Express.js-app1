e273d3a96b4591e61b6653fb0db71833fa596da5
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-09-08 13:30:07 +0530

    added styles and contact us page

--- routes/shop.js
+++ routes/shop.js
@@ -6,6 +6,14 @@ const rootDir = require('../helpers/path')
 
 const router = express.Router()
 
+router.get('/contact-us',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','contact-us.html'))
+})
+
+router.post('/success',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','success.html'))
+})
+
 router.get('/',(req,res,next)=>{
     res.sendFile(path.join(rootDir,'views','shop.html'))
 })
